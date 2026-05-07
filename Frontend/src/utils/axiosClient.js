e9319b3af726b015a8a1891105555ba6393161a6
Nakul Kumar <149577160+Nakul-Kumar01@users.noreply.github.com>
2026-05-07 21:57:20 +0530

    Changed the backend

--- Frontend/src/utils/axiosClient.js
+++ Frontend/src/utils/axiosClient.js
@@ -4,7 +4,7 @@ import axios from "axios"
 
 // configuring the axios 
 const axiosClient =  axios.create({
-    baseURL: 'https://nakul126.onrender.com',  //base url of backend
+    baseURL: 'https://nakulkumar-1.onrender.com',  //base url of backend
     withCredentials: true,  // it means attach cookies with request
     headers: {
         'Content-Type': 'application/json'  // data ka format json hai
